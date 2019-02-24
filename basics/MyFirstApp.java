b74f08778fec05a4fbd13fb30b3599e9e0535cb8
haris <haris.tsipis@gmail.com>
2019-02-24 18:07:27 +0200

    First java class + gitignore additions

---
+++ basics/MyFirstApp.java
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Cipitech, S.A. or its affiliates. All rights reserved.
+ *
+ * Contributors:
+ *     Altin Cipi (haris.tsipis@gmail.com)
+ *******************************************************************************/
+
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+	System.out.println("I Rule!");
+	System.out.println("The World");
+    }
+}
