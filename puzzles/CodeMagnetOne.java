89960f15929a597773b48d28f840000a45f642c0
acipi <haris.tsipis@gmail.com>
2019-02-27 22:25:43 +0200

    added puzzles projects with the puzzles of the first chapter

---
+++ puzzles/CodeMagnetOne.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Cipitech, S.A. or its affiliates. All rights reserved.
+ *
+ * Contributors:
+ *     Altin Cipi (haris.tsipis@gmail.com)
+ *******************************************************************************/
+
+/**
+ * page 54/722
+ */
+public class CodeMagnetOne {
+
+    public static void main(String[] args) {
+	int x = 3;
+
+	while (x > 0) {
+	    if (x > 2) {
+		System.out.print("a");
+	    }
+
+	    if (x == 2) {
+		System.out.print("b c");
+	    }
+
+	    x = x - 1;
+
+	    System.out.print("-");
+
+	    if (x == 1) {
+		System.out.print("d");
+		x = x - 1;
+	    }
+	}
+    }
+
+}
---
+++ puzzles/PoolPuzzleOne.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Cipitech, S.A. or its affiliates. All rights reserved.
+ *
+ * Contributors:
+ *     Altin Cipi (haris.tsipis@gmail.com)
+ *******************************************************************************/
+
+/**
+ * page 58/722
+ */
+public class PoolPuzzleOne {
+
+    public static void main(String[] args) {
+	int x = 0;
+
+	while (x < 4) {
+	    System.out.print("a");
+	    if (x < 1) {
+		System.out.print(" ");
+	    }
+	    System.out.print("n");
+
+	    if (x > 1) {
+		System.out.print(" oyster");
+		x = x + 2;
+	    }
+
+	    if (x == 1) {
+		System.out.print("noys");
+	    }
+
+	    if (x < 1) {
+		System.out.print("oise");
+	    }
+
+	    System.out.println("");
+
+	    x = x + 1;
+	}
+    }
+
+}
